7e7751bb3f42e8b617c872eb5c7f0e670dda9a73
Marcin Demski <m.demski@outlook.com>
2019-09-02 18:34:23 +0200

    User entity

---
+++ src/main/java/pl/coderslab/workshop/twitter/model/User.java
@@ -0,0 +1,85 @@
+package pl.coderslab.workshop.twitter.model;
+
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends AbstractEntity{
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy="user")
+    private List<Tweet> tweets;
+
+    public User(String email, String password, String firstName, String lastName, List<Tweet> tweets) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.tweets = tweets;
+    }
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public void setTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", tweets=" + tweets +
+                "} " + super.toString();
+    }
+}
