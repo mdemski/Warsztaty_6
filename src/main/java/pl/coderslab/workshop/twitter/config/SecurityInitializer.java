644c9e58edb447eb030a48ebd944b926c9e1f688
Marcin Demski <m.demski@outlook.com>
2019-09-02 18:29:24 +0200

    SecurityInitializer file to introduce spring security configuration

---
+++ src/main/java/pl/coderslab/workshop/twitter/config/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package pl.coderslab.workshop.twitter.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
