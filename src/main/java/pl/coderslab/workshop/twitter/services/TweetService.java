e4235ac1203aff1cf0ce31d139ee2dc8741db2df
mdemski <m.demski@outlook.com>
2019-09-10 21:22:15 +0200

    Added new method. Change methods to return sorted results

--- src/main/java/pl/coderslab/workshop/twitter/services/TweetService.java
+++ src/main/java/pl/coderslab/workshop/twitter/services/TweetService.java
@@ -1,41 +1,81 @@
 package pl.coderslab.workshop.twitter.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.workshop.twitter.dto.TweetDTO;
+import pl.coderslab.workshop.twitter.model.AbstractEntity;
+import pl.coderslab.workshop.twitter.model.Comment;
 import pl.coderslab.workshop.twitter.model.Tweet;
-import pl.coderslab.workshop.twitter.model.User;
 import pl.coderslab.workshop.twitter.repositories.TweetRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class TweetService {
 
-    private TweetRepository tweetRepository;
+    private TweetRepository<AbstractEntity, Number> tweetRepository;
 
-    public TweetService(TweetRepository tweetRepository) {
+    public TweetService(TweetRepository<AbstractEntity, Number> tweetRepository) {
         this.tweetRepository = tweetRepository;
     }
 
-    public List<Tweet> findAllByUser(User user) {
-        List<Tweet> tweetList = tweetRepository.findAllByUser(user);
-        return tweetList;
+    public List<TweetDTO> findAllByUserId(Long userId) {
+        Page<Tweet> tweets = tweetRepository.findAllByUserId(new PageRequest(0, 20, new Sort(Sort.Direction.DESC, "created")), userId);
+        List<Tweet> content = tweets.getContent();
+        return content.stream().map(source -> {
+            TweetDTO dto = new TweetDTO();
+            dto.setId(source.getId());
+            dto.setText(source.getText());
+            dto.setCreated(source.getCreated());
+            dto.setUserName(source.getUser().getEmail());
+            return dto;
+        }).collect(Collectors.toList());
     }
 
-    public List<Tweet> findAllByUserEmail(String email) {
-        List<Tweet> tweetList = tweetRepository.findAllByUserEmail(email);
-        return tweetList;
+    public List<TweetDTO> findAllByCreatedGreaterThan(LocalDateTime dateTime) {
+        Page<Tweet> tweetList = tweetRepository.findAllByCreatedGreaterThan(new PageRequest(0, 20, new Sort(Sort.Direction.DESC, "created")), dateTime);
+        List<Tweet> content = tweetList.getContent();
+        return content.stream().map(source -> {
+            TweetDTO dto = new TweetDTO();
+            dto.setId(source.getId());
+            dto.setText(source.getText());
+            dto.setCreated(source.getCreated());
+            dto.setUserName(source.getUser().getEmail());
+            return dto;
+        }).collect(Collectors.toList());
     }
 
-    public List<Tweet> findAllByCreatedGreaterThan(LocalDateTime dateTime) {
-        List<Tweet> tweetList = tweetRepository.findAllByCreatedGreaterThan(dateTime);
-        return tweetList;
+    public List<TweetDTO> findAllTweets() {
+        Page<Tweet> tweets = tweetRepository.findAll(new PageRequest(0, 20, new Sort(Sort.Direction.DESC, "created")));
+        List<Tweet> content = tweets.getContent();
+        return content.stream().map(source -> {
+            TweetDTO dto = new TweetDTO();
+            dto.setId(source.getId());
+            dto.setText(source.getText());
+            dto.setCreated(source.getCreated());
+            dto.setUserName(source.getUser().getEmail());
+            return dto;
+        }).collect(Collectors.toList());
     }
 
-    public List<Tweet> findAllTweets(){
-        List<Tweet> tweetList = tweetRepository.findAll();
-        return tweetList;
+    List<Comment> findAllByTweet(Tweet tweet) {
+        List<Comment> comments = findAllByTweet(tweet);
+        return comments;
+    }
+
+    public TweetDTO findOneByTweetId(Long id) {
+        Tweet tweet = tweetRepository.findOneById(id);
+        TweetDTO tweetDTO = new TweetDTO();
+        tweetDTO.setId(id);
+        tweetDTO.setUserName(tweet.getUser().getEmail());
+        tweetDTO.setText(tweet.getText());
+        tweetDTO.setCreated(tweet.getCreated());
+        return tweetDTO;
     }
 }
