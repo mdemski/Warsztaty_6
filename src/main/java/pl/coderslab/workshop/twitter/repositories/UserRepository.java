c77a9663f0d278046a1c3daacf77f06e193dd96f
mdemski <m.demski@outlook.com>
2019-09-10 21:20:49 +0200

    Added new method to repository.

--- src/main/java/pl/coderslab/workshop/twitter/repositories/UserRepository.java
+++ src/main/java/pl/coderslab/workshop/twitter/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import pl.coderslab.workshop.twitter.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     long countByEmail(String email);
+
+    User getByEmail(String email);
 }
