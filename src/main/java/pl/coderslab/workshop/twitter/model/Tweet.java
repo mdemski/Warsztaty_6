e9e5c6b3db2b7dc0123d4c76d2dc25f0078909e3
mdemski <m.demski@outlook.com>
2019-09-10 21:17:25 +0200

    Added comments to tweet with dependencies

--- src/main/java/pl/coderslab/workshop/twitter/model/Tweet.java
+++ src/main/java/pl/coderslab/workshop/twitter/model/Tweet.java
@@ -2,6 +2,7 @@ package pl.coderslab.workshop.twitter.model;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "tweets")
@@ -12,14 +13,25 @@ public class Tweet extends AbstractEntity {
     private User user;
     private String text;
     private LocalDateTime created;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tweet")
+    private List<Comment> comments;
 
-    public Tweet(User user, String text, LocalDateTime created) {
+    public Tweet(User user, String text, LocalDateTime created, List<Comment> comments) {
         this.user = user;
         this.text = text;
         this.created = created;
+        this.comments = comments;
     }
 
     public Tweet() {
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 
     public User getUser() {
@@ -46,12 +58,12 @@ public class Tweet extends AbstractEntity {
         this.created = created;
     }
 
-    @Override
-    public String toString() {
-        return "Tweet{" +
-                "user=" + user +
-                ", text='" + text + '\'' +
-                ", created=" + created +
-                "} " + super.toString();
-    }
+//    @Override
+//    public String toString() {
+//        return "Tweet{" +
+//                "user=" + user +
+//                ", text='" + text + '\'' +
+//                ", created=" + created +
+//                "} " + super.toString();
+//    }
 }
