834a8cd14a378d041ad911bdb3663793955c184e
mdemski <m.demski@outlook.com>
2019-09-15 10:09:24 +0200

    MessageDTO

---
+++ src/main/java/pl/coderslab/workshop/twitter/dto/MessageDTO.java
@@ -0,0 +1,73 @@
+package pl.coderslab.workshop.twitter.dto;
+
+import java.time.LocalDateTime;
+
+public class MessageDTO {
+
+    private Long id;
+    private String senderName;
+    private String recipientName;
+    private String title;
+    private String content;
+    private LocalDateTime created;
+    private boolean read;
+
+    public MessageDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+}
