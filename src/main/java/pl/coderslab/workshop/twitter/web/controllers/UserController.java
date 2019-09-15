a7b32e4496e1455fc129bd2303301fe7213716b2
mdemski <m.demski@outlook.com>
2019-09-15 10:07:56 +0200

    User controller with messages posted and received

--- src/main/java/pl/coderslab/workshop/twitter/web/controllers/UserController.java
+++ src/main/java/pl/coderslab/workshop/twitter/web/controllers/UserController.java
@@ -4,29 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.workshop.twitter.services.MessageService;
 import pl.coderslab.workshop.twitter.services.TweetService;
 import pl.coderslab.workshop.twitter.services.UserService;
 
 import java.security.Principal;
 
 @Controller
-@RequestMapping("/user-tweets")
-public class UserTweetsController {
+@RequestMapping("/user")
+public class UserController {
 
     private TweetService tweetService;
     private UserService userService;
+    private MessageService messageService;
 
-    public UserTweetsController(TweetService tweetService, UserService userService) {
+    public UserController(TweetService tweetService, UserService userService, MessageService messageService) {
         this.tweetService = tweetService;
         this.userService = userService;
+        this.messageService = messageService;
     }
 
     @GetMapping("/")
-    public String prepareAllTweetsByUser(Principal principal, Model model) {
+    public String prepareAllInfoByUser(Principal principal, Model model) {
         String userName = principal.getName();
         Long userId = userService.getUserByEmail(userName).getId();
         model.addAttribute("tweets", tweetService.findAllByUserId(userId));
-        return "all-user-tweets";
+        model.addAttribute("received", messageService.findAllByUserIdReceived(userId));
+        model.addAttribute("posted", messageService.findAllByUserIdPosted(userId));
+        return "all-user-info";
     }
 
 }
